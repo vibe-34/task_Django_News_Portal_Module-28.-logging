e0f5311122008477c460605665e5e3e41f37a2a1
vibe-34 <vibe.34@gmail.com>
2025-02-27 21:14:50 +0300

    Добавлена настройка  STATIC_ROOT

--- project/project/settings.py
+++ project/project/settings.py
@@ -137,13 +137,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR / "static"]  # для подгрузки стилей из папки static
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS = [BASE_DIR / "static"]  # для подгрузки стилей из папки static
 
 LOGIN_REDIRECT_URL = '/'  # После входа, нас перебросит на страницу ЛК (index.html)
 # LOGOUT_REDIRECT_URL = 'post/'                            # после выхода, перекинет по адресу(по default на стр. входа)
